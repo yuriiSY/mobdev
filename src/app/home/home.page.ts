adc58749736c33a0cde701078be108e91cd53a1f
Yurii Sykal <108358132+yuriiSY@users.noreply.github.com>
2023-06-26 16:18:02 +0100

    weather app

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,19 @@
+import { environment } from 'src/environments/environment';
 import { Component } from '@angular/core';
+import { HttpClient } from "@angular/common/http"
+
+const API_KEY = environment.API_KEY;
+const API_URL = environment.API_URL
+
+interface WeatherResponse {
+  main: {
+    temp: number;
+    humidity: number;
+    pressure: number;
+  };
+  // Other properties from the response if needed
+}
+
 
 @Component({
   selector: 'app-home',
@@ -7,6 +22,17 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  weatherValues :any
 
+  constructor(public httpClient: HttpClient) {
+    this.loadData()
+  }
+
+
+  loadData() { 
+    this.httpClient.get<WeatherResponse>(`${API_URL}/weather?q=${"Dublin"}&appid=${API_KEY}`).subscribe(result => {
+      console.log(result);
+      this.weatherValues = result.main
+     });
+  }
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,9 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  API_KEY: '985bde9c5aaf150542c3c20a7be5d4cb',
+  API_URL: 'https://api.openweathermap.org/data/2.5'
 };
 
 /*
