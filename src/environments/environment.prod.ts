9e1fa3cac1a613cf083e3f9327dfcb2abfcd8774
Yurii Sykal <108358132+yuriiSY@users.noreply.github.com>
2023-06-26 16:20:02 +0100

    app fix

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  API_KEY: '',
+  API_URL: 'https://api.openweathermap.org/data/2.5'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  API_KEY: '985bde9c5aaf150542c3c20a7be5d4cb',
+  API_KEY: '',
   API_URL: 'https://api.openweathermap.org/data/2.5'
 };
 
